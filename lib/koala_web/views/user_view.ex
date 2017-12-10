538015989b5b2e7afc0932addaf7cd97d0c6d76c
Nicanor Perera <nicanorperera@gmail.com>
2017-12-10 14:01:16 +1300

    Better form for user

--- lib/koala_web/views/user_view.ex
+++ lib/koala_web/views/user_view.ex
@@ -1,3 +1,19 @@
 defmodule KoalaWeb.UserView do
   use KoalaWeb, :view
+
+  # def role_name(type) do
+  #   valid_roles()
+  #   |> Enum.map(fn {key, value} -> {value, key} end)
+  #   |> Enum.into(%{})
+  #   |> Map.fetch!(type)
+  # end
+
+  def valid_roles do
+    [
+      {dgettext("accounts", "Admin"), "admin"},
+      {dgettext("accounts", "Editor"), "editor"},
+      {dgettext("accounts", "Guest"), "guest"}
+    ]
+  end
+
 end
